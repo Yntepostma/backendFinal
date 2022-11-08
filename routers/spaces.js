085fdddbfafb5623d4339346c8e103e98e7bffd4
YntepostmaE <ynte.postma@gmail.com>
2022-11-08 14:44:03 +0100

    feature 6 finished

--- routers/spaces.js
+++ routers/spaces.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const Space = require("../models").space;
 const Story = require("../models").story;
+const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
 
@@ -10,6 +11,14 @@ router.get("/", async (req, res, next) => {
     order: [[{ model: Story }, "createdAt", "DESC"]],
   });
   res.json(spacesWithStories);
+});
+
+router.put("/:id", authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const spaceToUpdate = await Space.findByPk(id);
+  console.log("space", spaceToUpdate);
+  const updatedSPace = await spaceToUpdate.update(req.body);
+  res.send(updatedSPace);
 });
 
 module.exports = router;
--- routers/stories.js
+++ routers/stories.js
@@ -1,24 +1,25 @@
 const { Router, application } = require("express");
 const Story = require("../models").story;
+const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
 
-router.post("/", async (req, res) => {
+router.post("/", authMiddleware, async (req, res) => {
   const { name, content, imageUrl, spaceId } = req.body;
   const newStory = await Story.create({ name, content, imageUrl, spaceId });
-  res.send(newStory);
+  res.send("Succes! New story was created");
 });
 
-router.delete("/", async (req, res) => {
-  console.log("req.params", req.body);
-  const { id } = req.body;
-
+router.delete("/:id", async (req, res) => {
+  //   console.log("req.params", req.body);
+  const { id } = req.params; //request from params
+  //   console.log("id", id);
   const storyToDelete = await Story.findByPk(id);
   if (!storyToDelete) {
     res.status(400).send("story not found");
   } else {
     await storyToDelete.destroy();
-    res.sendStatus(200);
+    res.status(200).send({ message: `Item with ${id} was deleted` });
   }
 });
 
